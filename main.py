b0ceafcd639f2c0ce4042460295d44a584ef1749
JosephJeongs <jeongjoseph46@gmail.com>
2023-10-10 09:10:00 -0500

    finished product

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 Builder.load_file("LoginPage.kv")
 
-users = {"Italianplayer":"Fr3shpa$ta","CatMouseChase":"Tom&J3rry"}
+users = {"Italian_player":"Fr3sh_pa$ta","CatMouseChase":"Tom&J3rry"}
 
 class LoginPageApp(App):
     def build(self):
@@ -13,39 +13,40 @@ class LoginManager(ScreenManager):
     pass
 
 class LoginPageScreen(Screen):
-
     def userinfo(self,username,password):
-            if username in users and password == users[username]:
-                self.manager.current = "loggedinpage"
-    def accountregister(self,bool):
+        if username in users and password == users[username]:
+            self.manager.current = "logged_in_page"
+        else:
+            self.ids.invalid_info.text = "Username or Password incorrect"
+            self.ids.invalid_info.color = "red"
+
+    def account_register(self,bool):
         if bool:
-            self.manager.current = "registerpage"
+            self.manager.current = "register_page"
 
 
 
 class AccountRegisterScreen(Screen):
-    def createaccount(self,user,password,confirmpassword):
+    def create_account(self,user,password,confirm_password):
         numbers = "1234567890"
-        specialchar = "~!@#$%^&*"
-        if user not in users:
-            for i in numbers:
-                for j in specialchar:
-                    if i in password:
-                        if j in password:
-                            if password.isupper() == False and password.islower() == False:
-                                if len(user)>=8 and len(password)>=8:
-                                    if confirmpassword == password:
-                                        self.manager.current = "loginpage"
+        special_char = "~!@#$%^&*_"
+        for i in numbers:
+            for j in special_char:
+                if user not in users and i in password and j in password and password.isupper() == False and password.islower() == False and len(user)>=8 and len(password)>=8 and confirm_password == password:
+                    users[user] = password
+                    self.manager.current = "login_page"
+                else:
+                    self.ids.invalid_info.text = "Invalid"
+                    self.ids.invalid_info.color = "red"
+
 
     def goback(self,bool):
         if bool:
-            self.manager.current = "loginpage"
-
-
+            self.manager.current = "login_page"
 
 class LoggedInScreen(Screen):
     def logout(self,bool):
         if bool:
-            self.manager.current = "loginpage"
+            self.manager.current = "login_page"
 
 LoginPageApp().run()
